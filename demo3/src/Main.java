382f7952b99d53379fd3025e6ce125cdf6c76bc5
bendangmi <1474051104@qq.com>
2022-05-20 08:58:57 +0800

    测试

--- demo3/src/Main.java
+++ demo3/src/Main.java
@@ -1,10 +1,31 @@
 /**
  * @author 本当迷
- * @Description TODO
+ * @Description
  * @date 2022/5/19-22:12
  */
 public class Main {
     public static void main(String[] args) {
+        Main main = new Main();
+        main.print();
 
+        new del("本当迷 ", 19).printInfo();
+    }
+    public void print(){
+        System.out.println("1122");
+    }
+
+    public static class del{
+        String name;
+        int age;
+
+        public del(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void printInfo(){
+            System.out.println("姓名为：" + name + "年龄为：" + age);
+        }
     }
 }
+
